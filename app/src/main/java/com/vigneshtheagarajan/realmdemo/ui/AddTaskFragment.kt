74f6b6eb6d8da9284d46277e4cbb4f32b698443d
Neo <c.min.tseng@gmail.com>
2024-02-14 13:56:55 +0800

    remove "kotlin-android-extensions" use ViewBinding replace

--- app/src/main/java/com/vigneshtheagarajan/realmdemo/ui/AddTaskFragment.kt
+++ app/src/main/java/com/vigneshtheagarajan/realmdemo/ui/AddTaskFragment.kt
@@ -1,35 +1,26 @@
 package com.vigneshtheagarajan.realmdemo.ui
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.vigneshtheagarajan.realmdemo.R
-import com.vigneshtheagarajan.realmdemo.model.Task
+import com.vigneshtheagarajan.realmdemo.databinding.AddTaskFragmentBinding
 import com.vigneshtheagarajan.realmdemo.util.RoundedBottomSheetDialogFragment
 import com.vigneshtheagarajan.realmdemo.viewModel.AddTaskViewModel
-import io.realm.Realm
-import kotlinx.android.synthetic.main.add_task_fragment.*
-import kotlin.math.log
 
 class AddTaskFragment : RoundedBottomSheetDialogFragment() {
-
+    private var _binding: AddTaskFragmentBinding? = null
+    private val binding get() = _binding!!
     companion object {
         fun newInstance() = AddTaskFragment()
     }
 
     private lateinit var viewModel: AddTaskViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.add_task_fragment, container, false)
+    override fun onCreateView(  inflater: LayoutInflater, container: ViewGroup?,  savedInstanceState: Bundle? ): View? {
+        _binding = AddTaskFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -39,10 +30,12 @@ class AddTaskFragment : RoundedBottomSheetDialogFragment() {
     }
 
     private fun listner() {
-        btn_save.setOnClickListener {
-            viewModel.saveTask(title.text.toString(), des.text.toString())
+        binding.btnSave.setOnClickListener {
+            viewModel.saveTask(binding.title.text.toString(), binding.des.text.toString())
         }
-
     }
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/vigneshtheagarajan/realmdemo/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/vigneshtheagarajan/realmdemo/ui/adapter/TaskAdapter.kt
@@ -1,18 +1,18 @@
 package com.vigneshtheagarajan.realmdemo.ui.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.vigneshtheagarajan.realmdemo.R
+import com.vigneshtheagarajan.realmdemo.databinding.ItemCellBinding
 import com.vigneshtheagarajan.realmdemo.model.Task
-import kotlinx.android.synthetic.main.layout_cell.view.*
 
-class TaskAdapter(val contactList: ArrayList<Task>) : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+class TaskAdapter(val contactList: ArrayList<Task>) :
+    RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.layout_cell, parent, false)
-        return ViewHolder(v)
+        val itemBinding = ItemCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val v = itemBinding.root
+        return ViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(holder: TaskAdapter.ViewHolder, position: Int) {
@@ -23,14 +23,14 @@ class TaskAdapter(val contactList: ArrayList<Task>) : RecyclerView.Adapter<TaskA
         return contactList.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(private val itemBinding: ItemCellBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
 
         fun bindItems(user: Task) {
             itemView.apply {
-                title.text = user.tilte
-                des.text = user.description
+                itemBinding.title.text = user.tilte
+                itemBinding.des.text = user.description
             }
-
         }
     }
 }
